f08bea258cb45abf4c14ad02ab7af57fb08f63ca
Alonso <alonso.rodriguez@udc.es>
2020-04-19 17:39:00 +0200

    p6 shellcheck final

--- p6/test.sh
+++ p6/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e # exit on error
 
@@ -7,7 +7,7 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-pushd $SCRIPTPATH
+pushd "$SCRIPTPATH" > /dev/null
 
 # Test programs
 if ! [ -x "$(command -v ghdl)" ]; then
@@ -28,14 +28,14 @@ ghdl -e tb_practica1
 ghdl -r tb_practica1 --vcd=tb_practica1_wave.vcd
 
 
-gtkwave tb_practica1_wave.vcd
+gtkwave tb_practica1_wave.vcd 2> /dev/null
 
-popd
+popd > /dev/null
 
 echo '<ENTER> para eliminar los archivos'
 read -r
 
-pushd $SCRIPTPATH
+pushd "$SCRIPTPATH" > /dev/null
 
 # Eliminamos (de forma muy poco elegante) los elementos del gitignore
 rm ./*.o
@@ -45,4 +45,4 @@ rm ./tb_practica1
 rm ./practica1
 rm ./tb_practica1_wave.vcd
 
-popd
+popd > /dev/null
