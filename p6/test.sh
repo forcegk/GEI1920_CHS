ff6f9bfc5e063a093649d313da751ae93b5494f3
Alonso <alonso.rodriguez@udc.es>
2020-04-19 17:32:38 +0200

    p6 shellcheck

--- p6/test.sh
+++ p6/test.sh
@@ -2,12 +2,17 @@
 
 set -e # exit on error
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 # Test programs
-if ![ -x "$(command -v ghdl)" ]; then
+if ! [ -x "$(command -v ghdl)" ]; then
     echo 'Error: ghdl is not installed.' >&2
     exit 1
 fi
-if ![ -x "$(command -v gtkwave)" ]; then
+if ! [ -x "$(command -v gtkwave)" ]; then
     echo 'Error: gtkwave is not installed.' >&2
     exit 1
 fi
@@ -24,12 +29,12 @@ ghdl -r tb_practica1 --vcd=tb_practica1_wave.vcd
 gtkwave tb_practica1_wave.vcd
 
 echo '<ENTER> para eliminar los archivos'
-read
+read -r
 
 # Eliminamos (de forma muy poco elegante) los elementos del gitignore
-rm *.o
-rm *.cf
+rm "$SCRIPTPATH"/*.o
+rm "$SCRIPTPATH"/*.cf
 
-rm tb_practica1
-rm practica1
-rm tb_practica1_wave.vcd
+rm "$SCRIPTPATH"/tb_practica1
+rm "$SCRIPTPATH"/practica1
+rm "$SCRIPTPATH"/tb_practica1_wave.vcd
