abac8488f0362d8b67df8d72f62551bb844d6587
Alonso <alonso.rodriguez@udc.es>
2020-04-20 02:04:50 +0200

    p7 all done menos informe

---
+++ p7/parte1/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e # exit on error
+
+
+IEEE_FLAGS=--ieee=synopsys
+
+
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+pushd "$SCRIPTPATH" > /dev/null
+
+# Test programs
+if ! [ -x "$(command -v ghdl)" ]; then
+    echo 'Error: ghdl is not installed.' >&2
+    exit 1
+fi
+if ! [ -x "$(command -v gtkwave)" ]; then
+    echo 'Error: gtkwave is not installed.' >&2
+    exit 1
+fi
+
+ghdl -a $IEEE_FLAGS filtro.vhd
+ghdl -a $IEEE_FLAGS TB_filtro1.vhd
+
+ghdl -e $IEEE_FLAGS filtro
+ghdl -e $IEEE_FLAGS tb_filtro1
+
+ghdl -r $IEEE_FLAGS tb_filtro1 --vcd=tb_filtro1_wave.vcd
+
+gtkwave tb_filtro1_wave.vcd 2> /dev/null
+
+popd > /dev/null
+
+echo '<ENTER> para eliminar los archivos'
+read -r
+
+pushd "$SCRIPTPATH" > /dev/null
+
+# Eliminamos (de forma muy poco elegante) los elementos del gitignore
+rm ./*.o
+rm ./*.cf
+
+rm ./filtro
+rm ./tb_filtro1
+rm ./tb_filtro1_wave.vcd
+
+popd > /dev/null
